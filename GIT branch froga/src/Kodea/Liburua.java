9b8b1b71b7d20370c44e8325fabdeb60e19f544d
AitorBasarrate <aitor.basarrateas@elorrieta-errekamari.com>
2019-03-07 13:54:25 +0100

    akkakakakak

--- GIT branch froga/src/Kodea/Liburua.java
+++ GIT branch froga/src/Kodea/Liburua.java
@@ -4,4 +4,24 @@ public class Liburua {
 	String izenburua;
 	String isbn;
 	int orriKop;
+	
+	
+	public String getIzenburua() {
+		return izenburua;
+	}
+	public void setIzenburua(String izenburua) {
+		this.izenburua = izenburua;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public int getOrriKop() {
+		return orriKop;
+	}
+	public void setOrriKop(int orriKop) {
+		this.orriKop = orriKop;
+	}
 }
--- GIT branch froga/src/Kodea/Liburutegia.java
+++ GIT branch froga/src/Kodea/Liburutegia.java
@@ -7,4 +7,30 @@ public class Liburutegia {
 		String helbidea;
 		int ikasle_Kop;
 		ArrayList<Liburua> liburuak;
+		
+		
+		public String getIzena() {
+			return izena;
+		}
+		public void setIzena(String izena) {
+			this.izena = izena;
+		}
+		public String getHelbidea() {
+			return helbidea;
+		}
+		public void setHelbidea(String helbidea) {
+			this.helbidea = helbidea;
+		}
+		public int getIkasle_Kop() {
+			return ikasle_Kop;
+		}
+		public void setIkasle_Kop(int ikasle_Kop) {
+			this.ikasle_Kop = ikasle_Kop;
+		}
+		public ArrayList<Liburua> getLiburuak() {
+			return liburuak;
+		}
+		public void setLiburuak(ArrayList<Liburua> liburuak) {
+			this.liburuak = liburuak;
+		}
 }
